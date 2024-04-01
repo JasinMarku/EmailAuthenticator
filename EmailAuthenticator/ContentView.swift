5ba649d76240150cfd0c142c9e2166dca65760e8
Jasin <jasinmarkuu@gmail.com>
2024-04-01 19:01:03 -0400

    Initial Commit

---
+++ EmailAuthenticator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmailAuthenticator
+//
+//  Created by Jasin ‎ on 4/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmailAuthenticator/EmailAuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmailAuthenticatorApp.swift
+//  EmailAuthenticator
+//
+//  Created by Jasin ‎ on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct EmailAuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
