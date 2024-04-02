b2c27082120bb7baa241baefafd6b5ae1bb5f1b2
Jasin <jasinmarkuu@gmail.com>
2024-04-02 10:51:26 -0400

    Email Verification using Firebase Authentication // First Official Commit

---
+++ EmailAuthenticator/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  EmailAuthenticator
+//
+//  Created by Jasin ‎ on 4/1/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
